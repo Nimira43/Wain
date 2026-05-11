565d847817b726a014d00bf54c1a1d3aa4b2a59a
Nimira43 <n.rabone72@aol.com>
2026-05-12 00:54:37 +0100

    Project setup.

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#018c01',
+        'main-dark': '#014701',
+        'main-light': '#7efa7e',
+        'dark': '#111',
+        'light': '#fafffa',
+        'grey-1': '#333',
+        'grey-2': '#999',
+        'grey-3': '#ccc',
+        'grey-4': '#eee',
+        'grey-5': '#fafafa',
+      }
+    },
+  },
+  plugins: [],
+}
+
